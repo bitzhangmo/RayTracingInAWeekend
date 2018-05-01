662bd560d6e5935c1d1e40bf6bd7d26508935c74
bitzhangmo <m15048619182@163.com>
2018-05-01 15:52:32 +0800

    << add the result of Chapter4

--- RayTracingInAWeekend/RayTracingInAWeekend/chapter3.cpp
+++ RayTracingInAWeekend/RayTracingInAWeekend/chapter3.cpp
@@ -10,10 +10,31 @@ vec3 color(const ray& r)
 	float t = 0.5*(unit_direction.y() + 1.0);//
 	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
+
+
+bool hit_sphere(const vec3& center, float radius, const ray& r)
+{
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0*dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+
+	float discrimnant = b * b - 4 * a*c;
+	return (discrimnant > 0);
+}
+
+vec3 color_Sphere(const ray& r)
+{
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+		return vec3(0, 1, 0);
+	vec3 unit_direction = unit_vector(r.direction());
+	float t = 0.5*(unit_direction.y() + 1.0);
+	return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 int main() {
 	int nx = 200;
 	int ny = 100;
-	ofstream outfile(".\\result\\thechapter3.txt", ios_base::out);
+	ofstream outfile(".\\result\\thechapter4.txt", ios_base::out);
 	outfile << "P3\n" << nx << " " << ny << "\n255\n";
 
 	vec3 lower_left_corner(-2.0, -1.0, -1.0);	//×óÏÂ½Ç
@@ -28,7 +49,7 @@ int main() {
 			float v = float(j) / float(ny);
 			
 			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 col=color(r);
+			vec3 col=color_Sphere(r);
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
